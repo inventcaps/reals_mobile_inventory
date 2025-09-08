0a293e482f45243d5aff4ea4c2f07b3bf1b4da69
ZuLu29 <markneilconde850@gmail.com>
2025-09-08 22:50:20 +0800

    add login page and dashboard

--- mobile_inventory/inventory/models.py
+++ mobile_inventory/inventory/models.py
@@ -1,3 +1,354 @@
 from django.db import models
 
-# Create your models here.
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Expenses(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+    description = models.TextField(blank=True, null=True)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'expenses'
+
+
+class HistoryLog(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    log_type = models.ForeignKey('HistoryLogTypes', models.DO_NOTHING)
+    log_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'history_log'
+
+
+class HistoryLogTypes(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=100)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'history_log_types'
+
+
+class Notifications(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    item_type = models.CharField(max_length=12)
+    item = models.ForeignKey('Products', models.DO_NOTHING)
+    notification_type = models.CharField(max_length=20)
+    notification_timestamp = models.DateTimeField()
+    is_read = models.BooleanField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'notifications'
+
+
+class ProductBatches(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    batch_date = models.DateField()
+    product = models.ForeignKey('Products', models.DO_NOTHING)
+    quantity = models.IntegerField()
+    manufactured_date = models.DateField()
+    expiration_date = models.DateField()
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product_batches'
+
+
+class ProductInventory(models.Model):
+    product = models.OneToOneField('Products', models.DO_NOTHING, primary_key=True)
+    total_stock = models.DecimalField(max_digits=10, decimal_places=2)
+    restock_threshold = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'product_inventory'
+
+
+class ProductRecipes(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey('Products', models.DO_NOTHING)
+    material = models.ForeignKey('RawMaterials', models.DO_NOTHING)
+    quantity_needed = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.ForeignKey('SizeUnits', models.DO_NOTHING)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product_recipes'
+
+
+class ProductTypes(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product_types'
+
+
+class ProductVariants(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product_variants'
+
+
+class Products(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product_type = models.ForeignKey(ProductTypes, models.DO_NOTHING)
+    variant = models.ForeignKey(ProductVariants, models.DO_NOTHING)
+    size = models.ForeignKey('Sizes', models.DO_NOTHING)
+    size_unit = models.ForeignKey('SizeUnits', models.DO_NOTHING)
+    unit_price = models.ForeignKey('UnitPrices', models.DO_NOTHING)
+    srp_price = models.ForeignKey('SrpPrices', models.DO_NOTHING)
+    description = models.TextField(blank=True, null=True)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'products'
+
+
+class RawMaterialBatches(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    batch_date = models.DateField()
+    material = models.ForeignKey('RawMaterials', models.DO_NOTHING)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    received_date = models.DateField()
+    expiration_date = models.DateField(blank=True, null=True)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'raw_material_batches'
+
+
+class RawMaterialInventory(models.Model):
+    material = models.OneToOneField('RawMaterials', models.DO_NOTHING, primary_key=True)
+    total_stock = models.DecimalField(max_digits=10, decimal_places=2)
+    reorder_threshold = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'raw_material_inventory'
+
+
+class RawMaterials(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    size = models.ForeignKey('Sizes', models.DO_NOTHING)
+    unit = models.ForeignKey('SizeUnits', models.DO_NOTHING)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    expiration_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'raw_materials'
+
+
+class Sales(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+    description = models.TextField(blank=True, null=True)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'sales'
+
+
+class SizeUnits(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    unit_name = models.CharField(max_length=45)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'size_units'
+
+
+class Sizes(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    size_label = models.CharField(max_length=255)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'sizes'
+
+
+class SrpPrices(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    srp_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'srp_prices'
+
+
+class StockChanges(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    item_type = models.CharField(max_length=12)
+    item_id = models.BigIntegerField()
+    quantity_change = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.TextField()
+    date = models.DateTimeField()
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'stock_changes'
+
+
+class UnitPrices(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'unit_prices'
+
+
+class Withdrawals(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    item_type = models.CharField(max_length=12)
+    item = models.ForeignKey(Products, models.DO_NOTHING)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    reason = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    created_by_admin = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'withdrawals'
