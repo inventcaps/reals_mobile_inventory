e0c9b8bb21e77f88ffe370b411ba38277244fdc9
ZuLu29 <markneilconde850@gmail.com>
2025-10-14 15:49:30 +0800

    Fix WhiteNoise static files storage for Render

--- mobile_inventory/mobile_inventory/settings.py
+++ mobile_inventory/mobile_inventory/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration for serving static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
